3f914772eb1d7691c32ce77f154d301248b37181
lilaeamedia <lilaeamedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-22 18:55:10 +0000

    initial release 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/intelliwidget-elements/tags/1.0.0@776889 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ intelliwidget-elements.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package IntelliWidget_Elements
+ * @version 1.0.0
+ */
+/*
+Plugin Name: IntelliWidget Elements Post Type
+Plugin URI: http://www.lilaeamedia.com/plugins/intelliwidget-elements
+Description: Adds generic non-searchable post type to be used for content areas
+Author: Lilaea Media LLC
+Version: 1.0.0
+Author URI: http://www.lilaeamedia.com
+*/
+
+if ( !defined('ABSPATH')) exit;
+
+/* Add non-searchable content elements.
+ * Use with IntelliWidget for maximum flexibility
+ * http://www.lilaeamedia.com/plugins/intelliwidget
+ */
+function intelliwidget_register_elements() {
+    register_post_type( 'element',
+        array(
+            'labels' => array(
+                'name'    => __('Elements',     'tiberius'),
+                'singular_name'   => __('Element',      'tiberius'),
+                'add_new_item'    => __('Add Element',  'tiberius'),
+                'edit_item'       => __('Edit Element', 'tiberius'),
+                'new_item'    => __('New Element',  'tiberius'),
+                'view_item'       => __('View Element', 'tiberius'),
+            ),
+            'public'      => true,
+            'has_archive'     => false,
+            'exclude_from_search' => true,
+            'publicly_queryable'  => false,
+            'show_ui'     => true,
+            'show_in_nav_menus'   => false,
+            'supports'    => array (
+                'title',
+                'editor',
+                'thumbnail',
+                'excerpt',
+                'page-attributes',
+                'custom-fields',
+            ),
+        )
+    );
+}
+
+add_action('init', 'intelliwidget_register_elements');
+?>
---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== IntelliWidget Elements Post Type ===
+Contributors: Jason Fleming
+Tags: intelliwidget, post type
+Requires at least: 3.5
+Tested up to: 3.6.1
+Stable tag: 1.0.0
+License: GPLv2
+
+== Description ==
+
+Adds generic non-searchable post type to be used with IntelliWidget for content areas.
+
+== Installation ==
+
+Upload to plugins or install from WP Admin and activate.
+
+== Frequently Asked Questions ==
+
+== Changelog ==
+
+= 1.0.0 =
+
+* Initial release
